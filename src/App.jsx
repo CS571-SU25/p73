2e696a93955e0d59ca912d75e806d88f5888ff5d
尹嘉熙的macbook <yinjiaxidemacbook@yinjiaxidemacbookdeMacBook-Air.local>
2025-07-21 13:26:32 -0500

    app setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { HashRouter, Routes, Route } from 'react-router'
+import Album from './components/Album'
+import Main from './components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <HashRouter>
+    <Routes>
+      <Route path="/main-page" element={<Main/>}></Route>
+      <Route path="/albums" element={<Album/>}></Route>
+    </Routes>
+  </HashRouter>
 }
 
 export default App
